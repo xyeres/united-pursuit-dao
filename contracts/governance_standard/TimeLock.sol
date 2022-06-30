05237e42d1d0cf550483e36a8373e8782606e45b
Michael Carr <xyeres@me.com>
2022-06-30 15:39:24 -0700

    setup governor contract

---
+++ contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+// Give time to users to "get out" if they don't like a governance
+
+import "@openzeppelin/contracts/governance/TimelockController.sol";
+
+contract TimeLock is TimelockController {
+  // minDelay: How long you have to wait before executing
+  // proposers is the list of addresses that can propose
+  // executors: Who can execute when a propsal passes
+  constructor(
+    uint256 minDelay,
+    address[] memory proposers,
+    address[] memory executors
+  ) TimelockController(minDelay, proposers, executors) {}
+}
