db39a601bf9182bbf2324936d5dad56ae2ddc4b7
Michael Carr <xyeres@me.com>
2022-07-01 15:44:16 -0700

    add basic script

--- hardhat-helper-config.ts
+++ hardhat-helper-config.ts
@@ -3,3 +3,9 @@ export const VOTING_PERIOD = 5;
 export const VOTING_DELAY = 1;
 export const QUORUM_PERCENTAGE = 4;
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
+export const NEW_STORE_VALUE = 77;
+export const FUNC = 'store';
+export const PROPOSAL_DESCRIPTION = `Change the value to ${NEW_STORE_VALUE}`;
+
+export const developmentChains = ["hardhat", "localhost"]; 
+export const proposalsFile = 'proposals.json'
